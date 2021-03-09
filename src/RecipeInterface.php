b00f1fb76c68e25042954f4f7c491a5a6481b65e
Steffen Rieke <dev@aenogym.de>
2021-03-09 15:14:49 +0100

    added MPL-2.0 license headers to source files

--- src/RecipeInterface.php
+++ src/RecipeInterface.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the m2devbox project.
+ * (c) Steffen Rieke <m2devbox@aenogym.de>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 namespace Devbox;
 
